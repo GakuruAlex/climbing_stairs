17975b1126c631de8dcef9186f70c0d1221ad345
= <=>
2025-03-08 16:18:15 +0300

    docs

---
+++ climbing_stairs.py
@@ -0,0 +1,9 @@
+def climbing_stairs(height: int) -> int:
+    """_Find how many ways there are to reach the top of a staircase if you can only take ascend one or two levels at a time_
+
+    Args:
+        height (int): _Number of stairs_
+
+    Returns:
+        int: _Number of ways to get to the top_
+    """
